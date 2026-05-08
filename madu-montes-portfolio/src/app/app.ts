de812289d33437fadd927b8b5a29020e121b96d1
madu-montes <monteseduarda56@gmail.com>
2026-05-07 23:11:11 -0300

    init: configurando o projeto

---
+++ madu-montes-portfolio/src/app/app.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Navbar,
+  Home,
+  Skills,
+  Projects,
+  Contact,
+  Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('madu-montes-portfolio');
+}
